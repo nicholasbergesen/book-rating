87d170bde92a77cff95c459de12d26c41650fd9b
Nicholas Bergesen <nicholasbergesen@github.com>
2023-04-05 23:16:14 +0900

    apples and eggs

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 function BookList({ books }) {
   const [bookRatings, setBookRatings] = useState({});
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     async function fetchBookRatings() {
@@ -16,13 +17,24 @@ function BookList({ books }) {
               .map((item) => item.volumeInfo?.averageRating)
               .filter((rating) => rating !== undefined && rating !== null)
               .sort((a, b) => a - b);
-            const rating = ratings.length > 0 ? `${ratings[0]}..${ratings[ratings.length - 1]}` : null;
-            return { [book.title]: rating };
+
+            if (ratings.length > 1) {
+              const minRating = ratings[0];
+              const maxRating = ratings[ratings.length - 1];
+              if (minRating === maxRating) return { [book.title]: minRating }; // no need to show range (e.g. 4..4
+              const rating = `${minRating}..${maxRating}`;
+              return { [book.title]: rating };
+            } else if (ratings.length === 1) {
+              return { [book.title]: ratings[0] };
+            } else {
+              return { [book.title]: -1 };
+            }
           })
       );
       const ratings = await Promise.all(promises);
       const ratingsMap = ratings.reduce((acc, rating) => ({ ...acc, ...rating }), {});
       setBookRatings(ratingsMap);
+      setLoaded(true);
     }
 
     fetchBookRatings();
@@ -31,29 +43,30 @@ function BookList({ books }) {
   const sortedBooks = books.sort((a, b) => {
     const aRating = bookRatings[a.title];
     const bRating = bookRatings[b.title];
-    if (aRating === null && bRating !== null) {
+
+    if (aRating == null && bRating != null) {
       return 1;
-    } else if (aRating !== null && bRating === null) {
+    } else if (aRating != null && bRating == null) {
       return -1;
-    } else if (aRating === null && bRating === null) {
+    } else if (aRating == null && bRating == null) {
       return 0;
     } else {
-      const aRatingMin = aRating?.split("..")[0] || 0;
-      const bRatingMin = bRating?.split("..")[0] || 0;
+      const aRatingMin = typeof aRating === 'string' ? aRating?.split("..")[0] : aRating || 0;
+      const bRatingMin = typeof bRating === 'string' ? bRating?.split("..")[0] : bRating || 0;
       return bRatingMin - aRatingMin;
     }
   });
 
 
-  return (
-    <ul>
+  return loaded ? (
+    <ul style={{listStyle: "none"}}>
       {sortedBooks.map((book) => (
         <li key={book.title}>
-          {book.title} ({bookRatings[book.title] !== null ? bookRatings[book.title] : "(not found)"})
+          {book.title} ({bookRatings[book.title] !== -1 ? bookRatings[book.title] : "no rating"})
         </li>
       ))}
     </ul>
-  );
+  ) : (<div>loading...</div>)
 }
 
 export default BookList
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BookList from './components/BookList';
-
-const books = [
-  { title: 'The Great Gatsby' },
-  { title: 'To Kill a Mockingbird' },
-  { title: '1984' },
-  { title: 'Pride and Prejudice' },
-  { title: 'The Catcher in the Rye' }
-];
+import App from './components/App';
 
 ReactDOM.render(
-  <BookList books={books} />,
+  <App />,
   document.getElementById('root')
 );
